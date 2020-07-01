3f35990d7df014595fad8b958354cf582fc70a9a
Joseph Chikwira <59054381+jchikwira@users.noreply.github.com>
2020-07-01 06:58:24 +0200

    Under Construction site

---
+++ Unic_Electrical_Solution/contact-form.handler.php
@@ -0,0 +1,21 @@
+﻿<?php
+$name =$_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+
+$email_form = 'info@unicelectrical.co.za';
+
+$email_subject = "New Form Submission";
+
+$email_body = "User Name: $name.\n"
+"User Name: $visitor_email.n";
+"User Message: $message.\n";
+
+$to = admin@unicelectrical.co.za";
+$headers ="From: $email_form \r\n";
+$headers ="Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: Index.html");
+   
